74fb38da4e18eb70310d76f3161de30202e7629f
frozenshadow <contact@leijlandia.nl>
2020-07-15 01:07:40 +0200

    Fix indents

--- src/LaravelOWMServiceProvider.php
+++ src/LaravelOWMServiceProvider.php
@@ -3,38 +3,34 @@ namespace Frozenshadow\LaravelOWM;
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelOWMServiceProvider extends ServiceProvider {
-
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-
-		$this->publishes([
-			__DIR__ . '/config' => config_path()
-		]);
-
-	}
-
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->bind('Frozenshadow\LaravelOWM\LaravelOWM', function(){
-
-			return new \Frozenshadow\LaravelOWM\LaravelOWM();
-
-		});
-	}
-
+class LaravelOWMServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
         // Loading routes file
         if (config('laravel-owm.routes_enabled')) {
             $this->loadRoutesFrom(__DIR__ . '/Http/routes.php');
         }
+
+        $this->publishes([
+            __DIR__ . '/config' => config_path()
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('Frozenshadow\LaravelOWM\LaravelOWM', function () {
+            return new LaravelOWM();
+        });
+    }
 }
